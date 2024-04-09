7f644f2188e4637f6733e1f7b02438b5dd5c9c09
magalli2715 <magallipuleciop@gmail.com>
2024-04-08 21:35:25 -0500

    actualizacion Javascript

--- ConceptosBasicos.js
+++ ConceptosBasicos.js
@@ -1,1 +1,37 @@
-console.log("hola mundo")
+console.log("hola mundo");
+
+
+let texto= "hola mundo"
+
+let numero= 7
+
+let esverdadero= false
+let esFalso= true
+
+let arregloNombres =["luisa","emanuel","paula","luciana" ]
+let arregloNumeros=[ 1,3,5,8,20]
+
+
+const Celular ={
+    color:"blanco",
+    modelo:"galaxy a7",
+    ancho:350,
+    alto:800,
+
+    enviarMensajes: function(){
+
+        console.log("Mensaje enviado");
+    },
+
+    sumar: function(){
+        console.log(5+4);
+    }
+}
+
+function buscarObjeto(){
+    console.log("le muestra lo que esta buscando")
+}
+function filtrarInformacion(){
+    console.log("le muestra solo informacion de la depresion")
+}
+
